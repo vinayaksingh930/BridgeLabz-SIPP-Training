308acdf33a317a51a57df38ee297354f1a29a8ac
Vinayak <vinsyak318@gmail.com>
2025-06-13 08:32:56 +0530

    Vinayak Day02 Level01 DivisibltyBy5 code setup

---
+++ Day02/Level01/DivisibltyBy5.java
@@ -0,0 +1,17 @@
+package Day02.Level01;
+
+import java.util.Scanner;
+
+public class DivisibltyBy5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        if (number % 5 == 0) {
+            System.out.println("Is the number " + number + " divisible by 5? Yes");
+        } else {
+            System.out.println("Is the number " + number + " divisible by 5? No");
+        }
+        scanner.close();
+    }
+}
